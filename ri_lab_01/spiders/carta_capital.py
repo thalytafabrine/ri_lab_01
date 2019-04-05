8e66041ef7b5e4f9c6f54fe9dbabcc68d4935e12
Thalyta Trindade <thalytaftrindade@gmail.com>
2019-04-05 18:31:25 +0100

    csv com resultados

--- ri_lab_01/spiders/carta_capital.py
+++ ri_lab_01/spiders/carta_capital.py
@@ -21,11 +21,22 @@ class CartaCapitalSpider(scrapy.Spider):
         self.start_urls = list(data.values())
     
     def getDateTime(self, response):
-        return response.css('div.eltdf-post-info-date > a::text')
+        data = response.xpath("//meta[@property='article:published_time']/@content").get()
+        if (data is not None):
+            data = data.replace("T", " ").split("+")[0]
+            data = datetime.strptime(data, '%Y-%m-%d %H:%M:%S')
+        return data
     
     def isValidDate(self, response):
-        date = response.css('div.eltdf-post-info-date a::attr(href)').get().split("/")[-3]
-        return int(date) >= 2018
+        date = self.getDateTime(response)
+        if (date is not None):
+            year = date.year
+            return year >= 2018
+        return False
+    
+    def getArticleDate(self, response):
+        date = self.getDateTime(response)
+        return date.strftime('%d/%m/%Y %H:%M:%S')
     
     def isLinkCartaCapital(self, link):
         for section in self.start_urls:
@@ -63,7 +74,7 @@ class CartaCapitalSpider(scrapy.Spider):
             'title': title,
             'sub_title': sub_title,
             'author': author,
-            'date': self.getDateTime(response),
+            'date': self.getArticleDate(response),
             'section': getSection(),
             'text': getText(),
             'url': response.url
